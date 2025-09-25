335c3614cc6fe989085f4814c06a7c5fa6934df3
michaelhtm <98621731+michaelhtm@users.noreply.github.com>
2025-09-25 12:12:28 -0700

    fix(rule): use ruleARN as adoption primary key
    Rule was requiring users to use listenerARN to adopt a resource.
    These changes ensure users provide the ruleARN to adopt a Rule

--- pkg/resource/rule/resource.go
+++ pkg/resource/rule/resource.go
@@ -87,21 +87,26 @@ func (r *resource) SetStatus(desired acktypes.AWSResource) {
 // SetIdentifiers sets the Spec or Status field that is referenced as the unique
 // resource identifier
 func (r *resource) SetIdentifiers(identifier *ackv1alpha1.AWSIdentifiers) error {
-	if identifier.NameOrID == "" {
-		return ackerrors.MissingNameIdentifier
+	if r.ko.Status.ACKResourceMetadata == nil {
+		r.ko.Status.ACKResourceMetadata = &ackv1alpha1.ResourceMetadata{}
 	}
-	r.ko.Spec.ListenerARN = &identifier.NameOrID
+	r.ko.Status.ACKResourceMetadata.ARN = identifier.ARN
 
 	return nil
 }
 
 // PopulateResourceFromAnnotation populates the fields passed from adoption annotation
 func (r *resource) PopulateResourceFromAnnotation(fields map[string]string) error {
-	primaryKey, ok := fields["listenerARN"]
+	resourceARN, ok := fields["arn"]
 	if !ok {
-		return ackerrors.NewTerminalError(fmt.Errorf("required field missing: listenerARN"))
+		return ackerrors.NewTerminalError(fmt.Errorf("required field missing: arn"))
 	}
-	r.ko.Spec.ListenerARN = &primaryKey
+
+	if r.ko.Status.ACKResourceMetadata == nil {
+		r.ko.Status.ACKResourceMetadata = &ackv1alpha1.ResourceMetadata{}
+	}
+	arn := ackv1alpha1.AWSResourceName(resourceARN)
+	r.ko.Status.ACKResourceMetadata.ARN = &arn
 
 	return nil
 }
